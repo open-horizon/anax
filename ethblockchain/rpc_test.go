bc56629dcc8d262fd7c4a4f33eebacfca6daf08f
michael dye <m-github@divisive.info>
2017-09-10 21:14:13 -0600

    fixed invocations of tests that need HTTPClientFactory instances

--- ethblockchain/rpc_test.go
+++ ethblockchain/rpc_test.go
@@ -1,13 +1,13 @@
 package ethblockchain
 
 import (
-	// "fmt"
 	"testing"
 )
 
 func TestClient_Constructor(t *testing.T) {
-	rpc_client := RPC_Connection_Factory("localhost", 8545, "")
-	if c := RPC_Client_Factory(rpc_client); c == nil {
+	rpcClient := RPC_Connection_Factory("localhost", 8545, "")
+
+	if c := RPC_Client_Factory(httpClientFactory(t), rpcClient); c == nil {
 		t.Errorf("Factory returned nil, but should not.\n")
 	} else if c.Get_connection().Get_fullURL() != "http://localhost:8545" {
 		t.Errorf("Factory returned a client that does not point to the right connection URL: %v\n", c.Get_connection().Get_fullURL())
@@ -15,7 +15,8 @@ func TestClient_Constructor(t *testing.T) {
 }
 
 func TestBadClientConstructor(t *testing.T) {
-	if c := RPC_Client_Factory(nil); c != nil {
+
+	if c := RPC_Client_Factory(httpClientFactory(t), nil); c != nil {
 		t.Errorf("Factory did not return nil, but should have.\n")
 	}
 }
