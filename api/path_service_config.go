351808dbc6f228b6d2b79163757ae644166a26f0
jiportilla <ivanp@us.ibm.com>
2019-08-28 15:40:14 -0600

    Issue - 1167

--- api/path_service_config.go
+++ api/path_service_config.go
@@ -260,7 +260,7 @@ func CreateService(service *Service,
 	if err1 != nil || sdef == nil {
 		if *service.Arch == thisArch {
 			// failed with user defined arch
-			return errorhandler(NewAPIUserInputError(fmt.Sprintf("Unable to find the service definition using  %v/%v %v %v in the exchange.", *service.Org, *service.Url, vExp.Get_expression(), *service.Arch), "service")), nil, nil
+			return errorhandler(NewAPIUserInputError(fmt.Sprintf("Unable to find the service definition using  %v/%v %v %v in the exchange. Error in INPUT File, service not present in the agreement.", *service.Org, *service.Url, vExp.Get_expression(), *service.Arch), "service")), nil, nil
 		} else {
 			// try node's arch
 			sdef, _, err1 = getService(*service.Url, *service.Org, vExp.Get_expression(), thisArch)
@@ -268,7 +268,7 @@ func CreateService(service *Service,
 				if pDevice.Pattern != "" {
 					return errorhandler(NewAPIUserInputError(fmt.Sprintf("Unable to find the service definition using  %v/%v %v %v in the exchange. Please ensure all services referenced in the user input file are included in pattern %v.", *service.Org, *service.Url, vExp.Get_expression(), thisArch, pDevice.Pattern), "service")), nil, nil
 				}
-				return errorhandler(NewAPIUserInputError(fmt.Sprintf("Unable to find the service definition using  %v/%v %v %v in the exchange.", *service.Org, *service.Url, vExp.Get_expression(), thisArch), "service")), nil, nil
+				return errorhandler(NewAPIUserInputError(fmt.Sprintf("Unable to find the service definition using  %v/%v %v %v in the exchange. Error in INPUT File, service not present in the agreement.", *service.Org, *service.Url, vExp.Get_expression(), thisArch), "service")), nil, nil
 			}
 		}
 	}
