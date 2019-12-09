590c7ce447366343a2bd6866ba65684eef635d1a
Nadim <nadim.jabbour@ibm.com>
2019-12-09 17:10:38 -0500

    Issue 1248

---
+++ test/gov/api_key.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+function results {
+  if [ "$(echo "$1" | jq -r '.code')" != "ok" ]
+  then
+    echo -e "Error: $(echo "$1" | jq -r '.msg')"
+    exit 2
+  fi
+}
+
+if [ ${CERT_LOC} -eq "1" ]; then
+  CERT_VAR="--cacert /certs/css.crt"
+else
+  CERT_VAR=""
+fi
+
+MAIN_AUTH="iamapikey:${API_KEY}"
+
+HZN_EXCHANGE_NODE_AUTH="testNode:testToken"
+NODE_NAME="testNode"
+ORG_ID=${AGBOT_NAME//"-agbot"/}
+
+export HZN_EXCHANGE_URL="${EXCH_APP_HOST}"
+
+# Register services via the hzn dev exchange commands
+./hzn_dev_services.sh ${HZN_EXCHANGE_URL} ${MAIN_AUTH}
+if [ $? -ne 0 ]
+then
+    echo -e "hzn service and pattern registration with hzn dev failed."
+    exit 1
+fi
+
+hzn exchange node remove -f -n "$HZN_EXCHANGE_NODE_AUTH" -o "$ORG_ID" -u "$MAIN_AUTH" "$NODE_NAME"
+hzn exchange service remove -u $MAIN_AUTH -o $ORG_ID -f $ORG_ID/bluehorizon.network-service-cpu_1.0_amd64
+
+hzn exchange node create -n "$HZN_EXCHANGE_NODE_AUTH" -m "$NODE_NAME" -o "$ORG_ID" -u "$MAIN_AUTH"
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange node create failed for $ORG_ID."
+    unset HZN_EXCHANGE_URL
+    exit 2
+fi
+
+KEY_TEST_DIR="/tmp/keytest"
+mkdir -p $KEY_TEST_DIR
+
+cd $KEY_TEST_DIR
+ls *.key &> /dev/null
+if [ $? -eq 0 ]
+then
+    echo -e "Using existing key"
+else
+  echo -e "Generate new signing keys:"
+  hzn key create -l 4096 "$ORG_ID" "$ORG_ID@gmail.com" -d .
+  if [ $? -ne 0 ]
+  then
+    echo -e "hzn key create failed."
+    exit 2
+  fi
+fi
+
+echo -e "Copy public key into anax folder:"
+cp $KEY_TEST_DIR/*public.pem /root/.colonus/. &> /dev/null
+
+# cpu service
+VERS="1.0"
+cat <<EOF >$KEY_TEST_DIR/svc_cpu.json
+{
+  "label":"CPU service",
+  "description":"CPU service",
+  "public":true,
+  "url":"https://bluehorizon.network/service-cpu",
+  "version":"$VERS",
+  "arch":"amd64",
+  "sharable":"singleton",
+  "matchHardware":{},
+  "userInput":[
+    {
+      "name":"cpu_var1",
+      "label":"",
+      "type":"string"
+    }
+  ],
+  "deployment":{
+    "services":{
+      "cpu":{
+        "image":"openhorizon/amd64_cpu:1.2.2"
+      }
+    }
+  },
+  "deploymentSignature":""
+}
+EOF
+
+echo -e "Register $ORG_ID/cpu service $VERS:"
+hzn exchange service publish -I -u $MAIN_AUTH -o $ORG_ID -f $KEY_TEST_DIR/svc_cpu.json -k $KEY_TEST_DIR/*private.key
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange service publish failed for $ORG_ID/cpu."
+    exit 2
+fi
+
+echo "Display IBM Org Pattern List"
+hzn exchange pattern list -n "$HZN_EXCHANGE_NODE_AUTH" -o "$ORG_ID" -u "$MAIN_AUTH" IBM/*
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange pattern list failed for IBM Org."
+    exit 2
+fi
+
+echo "Display $ORG_ID User List"
+hzn exchange user list -o "$ORG_ID" -u "$MAIN_AUTH"
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange user list failed."
+    exit 2
+fi
+
+echo -e "Delete $ORG_ID/cpu service $VERS:"
+hzn exchange service remove -u $MAIN_AUTH -o $ORG_ID -f $ORG_ID/bluehorizon.network-service-cpu_1.0_amd64
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange service publish failed for $ORG_ID/cpu."
+    exit 2
+fi
+
+echo -e "Delete node $HZN_EXCHANGE_NODE_AUTH"
+hzn exchange node remove -f -n "$HZN_EXCHANGE_NODE_AUTH" -o "$ORG_ID" -u "$MAIN_AUTH" "$NODE_NAME"
+if [ $? -ne 0 ]
+then
+    echo -e "hzn exchange node delete failed for $ORG_ID."
+    unset HZN_EXCHANGE_URL
+    exit 2
+fi
+
+unset HZN_EXCHANGE_URL
--- test/gov/gov-combined.sh
+++ test/gov/gov-combined.sh
@@ -128,8 +128,6 @@ else
   CERT_VAR=""
 fi
 
-sleep 20
-
 if [ "${EXCH_APP_HOST}" = "http://exchange-api:8080/v1" ]; then
   # Clean up the exchange DB to make sure we start out clean
   echo "Drop and recreate the exchange DB."
@@ -292,6 +290,17 @@ if [ $? -ne 0 ]
 then
   echo -e "Resource registration failure."
   exit -1
+fi
+
+# Start the API Key tests if it has been set
+if [ ${API_KEY} != "0" ]; then
+  echo -e "Starting API Key test."
+  ./api_key.sh
+  if [ $? -ne 0 ]
+  then
+    echo -e "API Key test failure."
+    exit -1
+  fi
 fi
 
 # test the CSS API
@@ -598,7 +607,7 @@ if [ "$NOCOMPCHECK" != "1" ]; then
 fi
 
 if [ "$NOSURFERR" != "1" ]; then
-  if [ "$TEST_PATTERNS" == "sall" ] || [ "$TEST_PATTERNS" == "" ] && [ "$NOLOOP" == "1" ] && [ "$NONS" == "" ] && [ "$NOGPS" == "" ] && [ "$NOPWS" == "" ] && [ "$NOLOC" == "" ] && [ "$NOHELLO" == "" ]; then
+  if [ "$TEST_PATTERNS" == "sall" ] || [ "$TEST_PATTERNS" == "" ] && [ "$NOLOOP" == "1" ] && [ "$NONS" == "" ] && [ "$NOGPS" == "" ] && [ "$NOPWS" == "" ] && [ "$NOLOC" == "" ] && [ "$NOHELLO" == "" ] && [ "${EXCH_APP_HOST}" == "http://exchange-api:8080/v1" ]; then
     ./verify_surfaced_error.sh
     if [ $? -ne 0 ]; then echo "Verify surfaced error failure."; exit 1; fi
   fi
@@ -608,9 +617,9 @@ if [ "$NOHZNREG" != "1" ]; then
   if [ "$TEST_PATTERNS" == "sall" ]; then
     echo "Sleeping 15 seconds..."
     sleep 15
-    
-    ./hzn_reg.sh 
-    if [ $? -ne 0 ]; then 
+
+    ./hzn_reg.sh
+    if [ $? -ne 0 ]; then
       echo "Failed registering and unregitering tests with hzn commands."
       exit 1
     fi
@@ -619,8 +628,8 @@ fi
 
 if [ "$NOPATTERNCHANGE" != "1" ]; then
   if [ "$TEST_PATTERNS" == "sall" ]; then
-    ./pattern_change.sh 
-    if [ $? -ne 0 ]; then 
+    ./pattern_change.sh
+    if [ $? -ne 0 ]; then
       echo "Failed node pattern change tests."
       exit 1
     fi
