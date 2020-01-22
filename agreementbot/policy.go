868c4b62a0b28dd04efdf8b13d627aa8f9c24ef4
Dave Booz <scabooz@gmail.com>
2020-01-22 13:00:48 -0500

    Issue 1308 - agbot handles object service ref changes

--- agreementbot/policy.go
+++ agreementbot/policy.go
@@ -15,7 +15,9 @@ import (
 	"strings"
 )
 
-func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.ObjectDestinationPolicies, nodeId string, nodePolicy *policy.Policy) error {
+// This function is called when an object is ready to be deployed to a node. It will perform the policy compatibility test
+// if necessary and will then update the object's destination list in the CSS.
+func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.ObjectDestinationPolicies, nodeId string, nodePolicy *policy.Policy, knownCompatible bool) error {
 
 	if len(*objPolicies) == 0 {
 		return nil
@@ -27,7 +29,8 @@ func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.Objec
 
 	currentObjDestinations := new(exchange.ObjectDestinationStatuses)
 
-	// For each object policy received, make sure it is still valid and then evaluate it against the node policy.
+	// For each object policy received, make sure the object is still valid, evaluate it against the node policy if necessary,
+	// and then update the object's destination list.
 	for _, objPol := range *objPolicies {
 
 		if obj, err := getObjectHandler(objPol.OrgID, objPol.ObjectID, objPol.ObjectType); err != nil {
@@ -37,27 +40,30 @@ func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.Objec
 			continue
 		}
 
-		glog.V(5).Infof(opLogstring(fmt.Sprintf("evaluating policy for object %v of type %v", objPol.ObjectID, objPol.ObjectType)))
+		// The caller might have already done the compatibility test.
+		if !knownCompatible {
+			glog.V(5).Infof(opLogstring(fmt.Sprintf("evaluating policy for object %v of type %v", objPol.ObjectID, objPol.ObjectType)))
 
-		// Evaluate the object policy against the edge node policy. If the object policy is compatible, then place the object
-		// on the node for the current agreement.
+			// Evaluate the object policy against the edge node policy. If the object policy is compatible, then place the object
+			// on the node for the current agreement.
 
-		// Convert the object's policy into an internal policy so that we can do the compatibility check.
-		internalObjPol := policy.Policy_Factory(fmt.Sprintf("object policy for %v type %v", objPol.ObjectID, objPol.ObjectType))
-		internalObjPol.Properties = objPol.DestinationPolicy.Properties
-		internalObjPol.Constraints = objPol.DestinationPolicy.Constraints
-		glog.V(5).Infof(opLogstring(fmt.Sprintf("converted object policy to: %v", internalObjPol)))
+			// Convert the object's policy into an internal policy so that we can do the compatibility check.
+			internalObjPol := policy.Policy_Factory(fmt.Sprintf("object policy for %v type %v", objPol.ObjectID, objPol.ObjectType))
+			internalObjPol.Properties = objPol.DestinationPolicy.Properties
+			internalObjPol.Constraints = objPol.DestinationPolicy.Constraints
+			glog.V(5).Infof(opLogstring(fmt.Sprintf("converted object policy to: %v", internalObjPol)))
 
-		// temporary fix - eliminate node constraints so that models can be deployed without repeating business policy
-		// properties plus service policy properties in the model policy properties.
-		nodePolicy.Constraints = []string{}
+			// temporary fix - eliminate node constraints so that models can be deployed without repeating business policy
+			// properties plus service policy properties in the model policy properties.
+			nodePolicy.Constraints = []string{}
 
-		// Check if node and model polices are compatible. Incompatible policies are not necessarily an error so just log a warning and return.
-		if err := policy.Are_Compatible(nodePolicy, internalObjPol, nil); err != nil {
-			glog.Warningf(opLogstring(fmt.Sprintf("error matching node policy %v and object policy %v, error: %v", nodePolicy, internalObjPol, err)))
-			return nil
-		} else {
-			glog.V(5).Infof(opLogstring(fmt.Sprintf("node %v is compatible with object %v with type %v", nodeId, objPol.ObjectID, objPol.ObjectType)))
+			// Check if node and model polices are compatible. Incompatible policies are not necessarily an error so just log a warning and return.
+			if err := policy.Are_Compatible(nodePolicy, internalObjPol, nil); err != nil {
+				glog.Warningf(opLogstring(fmt.Sprintf("error matching node policy %v and object policy %v, error: %v", nodePolicy, internalObjPol, err)))
+				return nil
+			} else {
+				glog.V(3).Infof(opLogstring(fmt.Sprintf("node %v is compatible with object %v/%v with type %v", nodeId, objPol.OrgID, objPol.ObjectID, objPol.ObjectType)))
+			}
 		}
 
 		// Grab the current destinations of the object.
@@ -67,7 +73,7 @@ func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.Objec
 			currentObjDestinations = dests
 		}
 
-		// Policies are compatible so place this object on the node. If the node we just made an agreement with is not in
+		// Policies are compatible so place this object on the node. If the node is not in
 		// the destination list of the object, add it.
 		pdlr := new(exchange.PutDestinationListRequest)
 		found := false
@@ -99,6 +105,8 @@ func AssignObjectToNode(ec exchange.ExchangeContext, objPolicies *exchange.Objec
 	return nil
 }
 
+// This function is called to remove an object from a node. It is assumed that the caller has already done the
+// policy compatibility check.
 func UnassignObjectFromNode(ec exchange.ExchangeContext, objPol *exchange.ObjectDestinationPolicy, nodeId string) error {
 
 	glog.V(5).Infof(opLogstring(fmt.Sprintf("unassign object %v %v %v from node %v", objPol.OrgID, objPol.ObjectType, objPol.ObjectID, nodeId)))
@@ -129,19 +137,20 @@ func UnassignObjectFromNode(ec exchange.ExchangeContext, objPol *exchange.Object
 		} else if err := updateDestHandler(objPol.OrgID, objPol, pdlr); err != nil {
 			glog.Errorf(opLogstring(fmt.Sprintf("%v", err)))
 		} else {
-			glog.V(3).Infof(opLogstring(fmt.Sprintf("updated destination list for object %v of type %v to remove node %v", objPol.ObjectID, objPol.ObjectType, nodeId)))
+			glog.V(3).Infof(opLogstring(fmt.Sprintf("updated destination list for object %v/%v of type %v to remove node %v", objPol.OrgID, objPol.ObjectID, objPol.ObjectType, nodeId)))
 		}
 	}
 	return nil
 }
 
-// MMS object policy changes can cause a significant impact to where objects are placed through the entire system.
+// MMS object policy changes can cause a significant impact to where objects are placed throughout the entire system.
+// Any aspect of the policy could have changed.
 // When an MMS object policy changes, it might mean one of the following:
 // 1. Nothing changes.
-//   a. A brand new policy is not eligible for any node on which there is already an agreement.
-//   b. A policy change is still not sufficent to make the object eligible for nodes that are already in an agreement.
-// 2. There are nodes on which the object should be removed.
-// 3. There are nodes on which the object should be placed, where there is already an agreement.
+//   a. A brand new policy is not eligible for any node on which the referenced service(s) are already running.
+//   b. A policy change is still not sufficent to make the object eligible for nodes that are running the referenced service(s).
+// 2. There are nodes from which the object should be removed.
+// 3. There are nodes on which the object should be placed, where the referenced service(s) are running.
 //   a. A new object/policy is placed on the node long after the agreement is in place.
 //   b. A policy change makes the object eligible for the node long after the agreement is in place.
 //
@@ -173,16 +182,8 @@ func (w *BaseAgreementWorker) HandleMMSObjectPolicy(cph ConsumerProtocolHandler,
 		return
 	}
 
-	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("OldPolicy: %v", oldPolicy)))
-	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("NewPolicy: %v", newPolicy)))
-
-	// Construct a list of service ids in the new policy.
-	newPolicyServiceKeys := make([]string, 0, 5)
-	for _, serviceID := range newPolicy.DestinationPolicy.Services {
-		newPolicyServiceKeys = append(newPolicyServiceKeys, cutil.FormOrgSpecUrl(serviceID.ServiceName, serviceID.OrgID))
-	}
-
-	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy NewPolicy service keys: %v", newPolicyServiceKeys)))
+	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy OldPolicy: %v", oldPolicy)))
+	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy NewPolicy: %v", newPolicy)))
 
 	// Construct a list of destinations where the object currently lives. These will be in the policy update (the new policy).
 	destNodes := make([]string, 0, 5)
@@ -206,114 +207,78 @@ func (w *BaseAgreementWorker) HandleMMSObjectPolicy(cph ConsumerProtocolHandler,
 		glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("Object Policy unable to read agreements, error %v", err)))
 	}
 
+	// Collect the set of nodes running services referenced in the new policy.
+	newTargetNodes := make(map[string]bool)
 	for _, agreement := range agreements {
+		for _, serviceId := range agreement.ServiceId {
 
-		// If an existing policy has changed, and the agreement's node is a current destination, then check to see if the node
-		// is still compatible with the new policy. If not, remove the node from the object's destination list.
-		if wi.Event.OldPolicy != nil && cutil.SliceContains(destNodes, exchange.GetId(agreement.DeviceId)) {
-
-			// There are 2 kinds of changes that could have occured. Something in the object's Service ID list could have
-			// changed (making none of the services on the node eligible for this object), OR the object's policy could
-			// have changed making the object incomaptible with the node policy. The former is checked first, then the
-			// latter.
-			found := false
-			for _, serviceId := range agreement.ServiceId {
-
-				if foundService, err := FindCompatibleServices(serviceId, &newPolicy, workerId, w.config.ArchSynonyms); err != nil {
-					// FindCompatibleServices logs it own errors.
-					continue
-				} else if foundService {
-					found = true
-					break
-				}
+			if foundService, err := FindCompatibleServices(serviceId, &newPolicy, workerId, w.config.ArchSynonyms); err != nil {
+				// FindCompatibleServices logs it own errors.
+				continue
+			} else if foundService {
+				newTargetNodes[agreement.DeviceId] = true
+				break
 			}
+		}
+	}
 
-			// If the current agreement is not compatible with the object policy because the services running on the
-			// node are no longer comaptible with the object policy, then make sure the object is not on the node.
-			if !found {
-				if err := UnassignObjectFromNode(w, &newPolicy, agreement.DeviceId); err != nil {
-					glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
-				}
-			} else {
+	// For nodes that are currently in the destination list, remove them if they are not running services
+	// in the policy.
+	for _, node := range destNodes {
+		nodeId := fmt.Sprintf("%v/%v", newPolicy.OrgID, node)
+		if _, ok := newTargetNodes[nodeId]; !ok {
+			if err := UnassignObjectFromNode(w, &newPolicy, nodeId); err != nil {
+				glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
+			}
+		}
+	}
 
-				// We found at least 1 service on the node that is compatible with the object's policy. However, the policy change
-				// could have been within the policy itself, making the node incompatible.
+	glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy checking nodes: %v", newTargetNodes)))
 
-				// Convert the object's policy into an internal policy so that we can do the compatibility check.
-				internalObjPol := policy.Policy_Factory(fmt.Sprintf("Object Policy for %v type %v", newPolicy.ObjectID, newPolicy.ObjectType))
-				internalObjPol.Properties = newPolicy.DestinationPolicy.Properties
-				internalObjPol.Constraints = newPolicy.DestinationPolicy.Constraints
-				glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy converted new object policy to: %v", internalObjPol)))
+	// We found at least 1 node with services in the object's policy. However, the policy change
+	// could have been within the policy itself, making the node incompatible.
+	for nodeId, _ := range newTargetNodes {
 
-				nodePolicyHandler := exchange.GetHTTPNodePolicyHandler(w)
-				_, nodePolicy, err := compcheck.GetNodePolicy(nodePolicyHandler, agreement.DeviceId, nil)
+		// Convert the object's policy into an internal policy so that we can do the compatibility check.
+		internalObjPol := policy.Policy_Factory(fmt.Sprintf("Object Policy for %v type %v", newPolicy.ObjectID, newPolicy.ObjectType))
+		internalObjPol.Properties = newPolicy.DestinationPolicy.Properties
+		internalObjPol.Constraints = newPolicy.DestinationPolicy.Constraints
+		glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy converted new object policy to: %v", internalObjPol)))
 
-				if err != nil {
-					glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
-				} else if nodePolicy == nil {
-					glog.Errorf(BAWlogstring(workerId, fmt.Errorf("No node policy found for %v", agreement.DeviceId)))
-				} else {
-					// temporary fix - eliminate node constraints so that models can be deployed without repeating business policy
-					// properties plus service policy properties in the model policy properties.
-					nodePolicy.Constraints = []string{}
+		nodePolicyHandler := exchange.GetHTTPNodePolicyHandler(w)
+		_, nodePolicy, err := compcheck.GetNodePolicy(nodePolicyHandler, nodeId, nil)
 
-					if err := policy.Are_Compatible(nodePolicy, internalObjPol, nil); err != nil {
-						// This agreement's node is no longer compatible, remove it from the destination list of the object.
-						if err := UnassignObjectFromNode(w, &newPolicy, agreement.DeviceId); err != nil {
-							glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
-						}
-					} else {
-						glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy node %v is still compatible with object %v with type %v", agreement.DeviceId, newPolicy.ObjectID, newPolicy.ObjectType)))
+		if err != nil {
+			glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
+		} else if nodePolicy == nil {
+			glog.Errorf(BAWlogstring(workerId, fmt.Errorf("No node policy found for %v", nodeId)))
+		} else {
+			// temporary fix - eliminate node constraints so that models can be deployed without repeating business policy
+			// properties plus service policy properties in the model policy properties.
+			nodePolicy.Constraints = []string{}
+
+			if err := policy.Are_Compatible(nodePolicy, internalObjPol, nil); err != nil {
+				// This node is not compatible, remove it from the destination list of the object, if necessary.
+				glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy node %v is not compatible with object %v with type %v", nodeId, newPolicy.ObjectID, newPolicy.ObjectType)))
+				if cutil.SliceContains(destNodes, exchange.GetId(nodeId)) {
+					if err := UnassignObjectFromNode(w, &newPolicy, nodeId); err != nil {
+						glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
 					}
 				}
-			}
-
-		} else {
-
-			// 1. Brand new policy - node might be eligble now
-			// 2. Updated policy - node might be eligible now
-
-			// If the agreement has a service id in the list of services for the newpolicy AND the agreement's node is not in the current dest list,
-			// then check to see if the new policy is compatible with node policy. If so, we might want to add the object to the node.
-			objPolicies := new(exchange.ObjectDestinationPolicies)
-			(*objPolicies) = append((*objPolicies), newPolicy)
-			for _, serviceId := range agreement.ServiceId {
-
-				agServiceIdPieces := strings.SplitN(serviceId, "_", 3)
-
-				if cutil.SliceContains(newPolicyServiceKeys, agServiceIdPieces[0]) && !cutil.SliceContains(destNodes, exchange.GetId(agreement.DeviceId)) {
-
-					if found, err := FindCompatibleServices(serviceId, &newPolicy, workerId, w.config.ArchSynonyms); err != nil {
-						// FindCompatibleServices logs it own errors.
-						continue
-					} else if found {
-						// Add the node to the object destination if eligible.
-						nodePolicyHandler := exchange.GetHTTPNodePolicyHandler(w)
-						_, nodePolicy, err := compcheck.GetNodePolicy(nodePolicyHandler, agreement.DeviceId, nil)
-
-						if err != nil {
-							glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("Object Policy error %v", err)))
-						} else if nodePolicy == nil {
-							glog.Errorf(BAWlogstring(workerId, fmt.Errorf("No node policy found for %v", agreement.DeviceId)))
-						} else {
-							// temporary fix - eliminate node constraints so that models can be deployed without repeating business policy
-							// properties plus service policy properties in the model policy properties.
-							nodePolicy.Constraints = []string{}
-
-							if err := AssignObjectToNode(w, objPolicies, agreement.DeviceId, nodePolicy); err != nil {
-								glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("Object Policy error %v", err)))
-							}
-						}
-
-						// As long as the node is running at least 1 service from the agreement, then place the object on the node.
-						// That means we can stop iterating the new policy's service ID list.
-						break
+			} else {
+				glog.V(5).Infof(BAWlogstring(workerId, fmt.Sprintf("Object Policy node %v is compatible with object %v with type %v", nodeId, newPolicy.ObjectID, newPolicy.ObjectType)))
+				if !cutil.SliceContains(destNodes, exchange.GetId(nodeId)) {
+					objPolicies := new(exchange.ObjectDestinationPolicies)
+					(*objPolicies) = append((*objPolicies), newPolicy)
+					if err := AssignObjectToNode(w, objPolicies, nodeId, nodePolicy, true); err != nil {
+						glog.Errorf(BAWlogstring(workerId, fmt.Sprintf("%v", err)))
 					}
 				}
 			}
 		}
-
 	}
+
+	glog.V(3).Infof(BAWlogstring(workerId, fmt.Sprintf("done with MMS Object Policy event: %v", wi)))
 
 	return
 
