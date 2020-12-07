b24ce50ee862cec135b325767703f3b61949fec9
MaxMcAdam <max@fredcom.com>
2020-12-07 08:54:20 -0800

    Issue #2191 - Support hubadmin for 'hzn exchange user create' command
    Signed-off-by: MaxMcAdam <max@fredcom.com>

--- cli/exchange/user.go
+++ cli/exchange/user.go
@@ -10,8 +10,17 @@ import (
 )
 
 type ExchangeUsers struct {
-	Users     map[string]interface{} `json:"users"`
-	ListIndex int                    `json:"lastIndex"`
+	Users     map[string]ExchangeUser `json:"users"`
+	ListIndex int                     `json:"lastIndex"`
+}
+
+type ExchangeUser struct {
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	Admin       bool   `json:"admin"`
+	HubAdmin    bool   `json:"hubAdmin"`
+	LastUpdated string `json:"lastUpdated"`
+	UpdatedBy   string `json:"updatedBy"`
 }
 
 func UserList(org, userPwCreds, theUser string, allUsers, namesOnly bool) {
@@ -53,16 +62,25 @@ func UserList(org, userPwCreds, theUser string, allUsers, namesOnly bool) {
 	}
 }
 
-func UserCreate(org, userPwCreds, user, pw, email string, isAdmin bool) {
+func UserCreate(org, userPwCreds, user, pw, email string, isAdmin bool, isHubAdmin bool) {
+	// get message printer
+	msgPrinter := i18n.GetMessagePrinter()
+
 	if email == "" {
 		if strings.Contains(user, "@") {
 			email = user
 		} else {
-			cliutils.Fatal(cliutils.CLI_INPUT_ERROR, i18n.GetMessagePrinter().Sprintf("the email must be specified via -e if the username is not an email address."))
+			cliutils.Fatal(cliutils.CLI_INPUT_ERROR, msgPrinter.Sprintf("the email must be specified via -e if the username is not an email address."))
 		}
 	}
+
+	if isHubAdmin && org != "root" {
+		cliutils.Fatal(cliutils.CLI_GENERAL_ERROR, msgPrinter.Sprintf("Only exchange users in the root org can be hubadmins."))
+	}
+
 	cliutils.SetWhetherUsingApiKey(userPwCreds)
-	postUserReq := cliutils.UserExchangeReq{Password: pw, Admin: isAdmin, Email: email}
+
+	postUserReq := cliutils.UserExchangeReq{Password: pw, Admin: isAdmin, HubAdmin: isHubAdmin, Email: email}
 	cliutils.ExchangePutPost("Exchange", http.MethodPost, cliutils.GetExchangeUrl(), "orgs/"+org+"/users/"+user, cliutils.OrgAndCreds(org, userPwCreds), []int{201}, postUserReq, nil)
 }
 
@@ -74,6 +92,39 @@ func UserSetAdmin(org, userPwCreds, user string, isAdmin bool) {
 	cliutils.SetWhetherUsingApiKey(userPwCreds)
 	patchUserReq := UserExchangePatchAdmin{Admin: isAdmin}
 	cliutils.ExchangePutPost("Exchange", http.MethodPatch, cliutils.GetExchangeUrl(), "orgs/"+org+"/users/"+user, cliutils.OrgAndCreds(org, userPwCreds), []int{201}, patchUserReq, nil)
+}
+
+func UserSetHubAdmin(org, userPwCreds, user string, isHubAdmin bool) {
+	// get message printer
+	msgPrinter := i18n.GetMessagePrinter()
+
+	_, userId := cliutils.TrimOrg(org, user)
+
+	// Get the user from the exchange
+	var userList ExchangeUsers
+	httpCode := cliutils.ExchangeGet("Exchange", cliutils.GetExchangeUrl(), "orgs/root/users/"+userId, cliutils.OrgAndCreds(org, userPwCreds), []int{200, 404, 403}, &userList)
+	if httpCode == 200 {
+		// If the user already exists in the root org, we need to update it with a PUT to set hubAdmin to correct state
+		exchUser := cliutils.AddOrg("root", userId)
+		if existingUser, ok := userList.Users[exchUser]; !ok {
+			cliutils.Fatal(cliutils.NOT_FOUND, msgPrinter.Sprintf("user '%s' not found in list returned from exchange", exchUser))
+		} else {
+			updatedUser := cliutils.UserExchangeReq{Password: existingUser.Password, Admin: existingUser.Admin, HubAdmin: isHubAdmin, Email: existingUser.Email}
+			cliutils.ExchangePutPost("Exchange", http.MethodPut, cliutils.GetExchangeUrl(), "orgs/root/users/"+userId, cliutils.OrgAndCreds(org, userPwCreds), []int{201}, updatedUser, nil)
+		}
+	} else if httpCode == 403 {
+		cliutils.Fatal(cliutils.CLI_GENERAL_ERROR, msgPrinter.Sprintf("Only hubadmin users and the exchange root user can create hubadmins."))
+	} else if isHubAdmin {
+		// If the user does not exist in the root org and the user isHubAdmin, create them with hubAdmin set to true and no other fields
+		postUserReq := cliutils.UserExchangeReq{HubAdmin: true}
+		httpCode := cliutils.ExchangePutPost("Exchange", http.MethodPost, cliutils.GetExchangeUrl(), "orgs/root/users/"+userId, cliutils.OrgAndCreds(org, userPwCreds), []int{201, 400, 403}, postUserReq, nil)
+		if httpCode == 400 {
+			cliutils.Fatal(cliutils.CLI_GENERAL_ERROR, msgPrinter.Sprintf("Hubadmin status cannot be set because the user %s does not exist in the root org.", user))
+		} else if httpCode == 403 {
+			cliutils.Fatal(cliutils.CLI_GENERAL_ERROR, msgPrinter.Sprintf("Only hubadmin users and the exchange root user can create hubadmins."))
+		}
+	}
+	// If the user does not exist in the root org and isHubAdmin is false, we don't need to do anything
 }
 
 func UserRemove(org, userPwCreds, user string, force bool) {
