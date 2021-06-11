7d370909aee38cbdda7ce3294ebc60246e7943f9
Ben Courliss <bencourliss@ibm.com>
2021-06-11 14:21:39 -0400

    Added additional model cleanup
    Signed-off-by: Ben Courliss <bencourliss@ibm.com>

--- test/gov/clean_css.sh
+++ test/gov/clean_css.sh
@@ -14,5 +14,18 @@ hzn mms object delete -t "model" -i "multires.tgz"
 hzn mms object delete -t "model" -i "policy-basicres.tgz"
 hzn mms object delete -t "model" -i "policy-multires.tgz"
 sleep 5
+hzn mms object delete -t "test" -i "test-medium1"
+hzn mms object delete -t "test" -i "test-large1"
+hzn mms object delete -t "test" -i "test1"
+hzn mms object delete -t "test" -i "test2"
+hzn mms object delete -t "test" -i "test3"
+hzn mms object delete -t "test" -i "test_user_access"
+sleep 5
+hzn mms object delete -t "test" -i "test-medium1"
+hzn mms object delete -t "test" -i "test-large1"
+hzn mms object delete -t "test" -i "test1"
+hzn mms object delete -t "test" -i "test2"
+hzn mms object delete -t "test" -i "test3"
+hzn mms object delete -t "test" -i "test_user_access"
 
 unset HZN_FSS_CSSURL
