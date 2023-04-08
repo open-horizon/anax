b2e55ccf9452eebb71bef3d6273e805809a3252f
Demopans <gao.jeff3@gmail.com>
2023-04-08 00:03:35 -0400

    use compose. try to get jetbrains gateway to behave for multiple reasons
    firstish
    
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

---
+++ dockerDev/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! grep -q ${SSH_USERNAME} /etc/group &> /dev/null; then
+	addgroup ${SSH_USERNAME}
+fi
+
+if ! id "${SSH_USERNAME}" &> /dev/null; then
+	adduser --home /opt/anax --ingroup ${SSH_USERNAME} ${SSH_USERNAME}
+fi
+
+echo -e "${SSH_PASSWORD}\n${SSH_PASSWORD}" | passwd ${SSH_USERNAME}
+chown -R ${SSH_USERNAME} /opt/anax
+
+service ssh start
+
+tail -f /dev/null
