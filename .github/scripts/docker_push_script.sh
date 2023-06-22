d3a16666be6d9096fcb0bf335a83aa4d428de388
blakep7 <pearsonblake9@gmail.com>
2023-06-22 10:50:48 -0700

    Issue #3822 - Feature Request: Create build pipeline to build and publish Anax artifacts
    Signed-off-by: blakep7 <pearsonblake9@gmail.com>

---
+++ .github/scripts/docker_push_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Names of the images created for each architecture
+if [[ ${arch} == 'amd64' ]]; then
+    images=('amd64_agbot' 'amd64_anax' 'amd64_anax_k8s' 'amd64_auto-upgrade-cronjob_k8s' 'amd64_cloud-sync-service' 'amd64_edge-sync-service')
+elif [[ ${arch} == 'ppc64el' ]]; then
+    images=('ppc64el_anax' 'ppc64el_anax_k8s' 'ppc64el_auto-upgrade-cronjob_k8s' 'ppc64el_edge-sync-service')
+elif [[ ${arch} == 'arm64' ]]; then
+    images=('arm64_anax' 'arm64_anax_k8s' 'arm64_auto-upgrade-cronjob_k8s' 'arm64_edge-sync-service')
+fi
+
+# Push those images
+for image in "${images[@]}"; do
+
+    if [[ ${image} == *"cloud-sync-service"* ]]; then
+        VERSION=${CSS_IMAGE_VERSION}
+    elif [[ ${image} == *"edge-sync-service"* ]]; then
+        VERSION=${ESS_IMAGE_VERSION}
+    else
+        VERSION=${ANAX_IMAGE_VERSION}
+    fi
+
+    if [[ ${GITHUB_REF} == 'refs/heads/master' ]]; then
+        docker push ${IMAGE_REPO}/${image}:testing
+    fi
+
+    if [[ -n "$VERSION" ]]; then
+        docker tag ${IMAGE_REPO}/${image}:testing ${IMAGE_REPO}/${image}:${VERSION}
+        docker push ${IMAGE_REPO}/${image}:${VERSION}
+    else
+        echo 'Error pushing docker images, variable $VERSION is unset'
+    fi
+
+done
