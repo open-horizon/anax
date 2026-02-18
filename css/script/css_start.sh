3d3c99c42a1d26be3bba085f158aa7c4f907185d
Nathan Phelps <naphelps@us.ibm.com>
2026-02-18 11:40:45 -0500

    something
    Signed-off-by: Nathan Phelps <naphelps@us.ibm.com>

--- css/script/css_start.sh
+++ css/script/css_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /usr/bin/envsubst < /etc/edge-sync-service/sync.conf.tmpl > /etc/edge-sync-service/sync.conf
-export UNIX_SOCKET_FILE_PERMISSIONS ?= 0o777
+export UNIX_SOCKET_FILE_PERMISSIONS=${UNIX_SOCKET_FILE_PERMISSIONS:-0o777}
 /home/cssuser/cloud-sync-service
